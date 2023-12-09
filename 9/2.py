2f1aad0e98f7c88e19509adba8420dd6fb7eed63
Chua Shao Shxuan <shaoshxuan.chua@gmail.com>
2023-12-10 02:05:34 +0800

    Advent of Code Day 9

---
+++ 9/2.py
@@ -0,0 +1,18 @@
+testFile = open('test.txt', 'r')
+testData = testFile.read().split('\n')
+
+def difference(data):
+  if all(ele == 0 for ele in data):
+    return 0
+  nextDiff = []
+  for i in range(len(data)-1):
+    nextDiff.append(int(data[i+1]) - int(data[i]))
+
+  return difference(nextDiff) + nextDiff[-1]
+
+total = 0
+for line in testData:
+  lineArr = line.split(" ")[::-1]
+  total += difference(lineArr) + int(lineArr[-1])
+
+print(total)
